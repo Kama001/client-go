da7cd3cc09e4faf7482f5ffa9f0db8d91e63b243
kcharan <kamana_sai.charan@mercedes-benz.com>
2025-04-17 22:05:24 +0530

    add configuration to run inside cluster

--- ekspose/main.go
+++ ekspose/main.go
@@ -3,17 +3,26 @@ package main
 import (
 	// "/lhome/kcharan/kubernetes_scaling/client-go/ekspose/customcontroller"
 	"flag"
+	"fmt"
 	"time"
 
 	"github.com/kama001/client-go/ekspose/customctrlwithqueue"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
 	kubeconfig := flag.String("kubeconfig", "/lhome/kcharan/.kube/config", "kubeconfig location")
-	config, _ := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		fmt.Printf("error building from flags %s", err.Error())
+		config, err = rest.InClusterConfig()
+		if err != nil {
+			fmt.Printf("error getting incluster config %s", err.Error())
+		}
+	}
 	clientset, _ := kubernetes.NewForConfig(config)
 	ch := make(chan struct{})
 	ns := "default"
